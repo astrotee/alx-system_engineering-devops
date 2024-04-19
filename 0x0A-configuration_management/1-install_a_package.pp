f41793e91b6bc737a81dff2cf557ebc1d0530305
amin ahmed <aminahmed24@gmail.com>
2024-04-19 22:10:12 +0200

    ensure werkzeug 2.1.1 is installed

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,4 +3,11 @@ package { 'flask':
     ensure   => '2.1.0',
     name     => 'flask',
     provider => 'pip3',
+    require  => Package['werkzeug'],
+}
+
+package { 'werkzeug':
+    ensure   => '2.1.1',
+    name     => 'werkzeug',
+    provider => 'pip3',
 }
