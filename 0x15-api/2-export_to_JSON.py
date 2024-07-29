bab37f7c47359e91aa89b78f49907313b3a7e286
amin ahmed <aminahmed24@gmail.com>
2024-07-29 23:20:03 +0300

    2. Export to JSON

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Export to CSV"""
+import sys
+import requests as req
+import json
+
+base_url = "https://jsonplaceholder.typicode.com"
+
+if __name__ == "__main__":
+    id = sys.argv[1]
+    res = req.get(f"{base_url}/users/{id}")
+    res = res.json()
+    username = res['username']
+    res = req.get(f"{base_url}/users/{id}/todos/")
+    res = res.json()
+    ntodos = len(res)
+    ndone = 0
+    tasks = list()
+    d = {f"{id}": tasks}
+    for todo in res:
+        tasks.append({"task": todo['title'], "completed": todo['completed'], "username": username})
+    with open(f"{id}.json", 'w') as f:
+        json.dump(d, f)
