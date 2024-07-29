9fcdf85a71b80d6ecde2e5577433db56f032a4fc
amin ahmed <aminahmed24@gmail.com>
2024-07-29 10:08:03 +0300

    0. Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Gather data from an API"""
+import sys
+import requests as req
+
+base_url = "https://jsonplaceholder.typicode.com"
+
+if __name__ == "__main__":
+    id = sys.argv[1]
+    res = req.get(f"{base_url}/users/{id}")
+    res = res.json()
+    name = res['name']
+    res = req.get(f"{base_url}/users/{id}/todos/")
+    res = res.json()
+    ntodos = len(res)
+    ndone = 0
+    done_tasks = []
+    for todo in res:
+        if todo['completed']:
+            ndone += 1
+            done_tasks.append(todo['title'])
+    print(f"Employee {name} is done with tasks({ndone}/{ntodos}):")
+    for title in done_tasks:
+        print(f"\t {title}")
